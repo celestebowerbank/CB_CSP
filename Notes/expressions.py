4c4be6ad11081631f8a1c6683c38d41b800e2e5c
celestebowerbank <celestebowerbank@ucas-udu.net>
2025-09-04 14:41:04 -0600

    Working on expression notes

---
+++ Notes/expressions.py
@@ -0,0 +1,45 @@
+# CB 7th Expression Notes
+
+# What is an algorithm? 
+    # A set of instructions to complete a task.
+name = input("What is your name:\n")
+print("Hello", name)
+
+#Treyson has 12 apples, he has 5 friends that he wants to give apples to. How many apples does each friend get? 
+apples = 12
+friends = 5
+print("Each friend gets", apples//friends, "apples!")
+
+# List steps in an algorithm 
+    # Declare Varables (Get needed info)
+    # Perform any equations - Do the things
+    # Display Results
+
+    # Average age of a group of 4 people
+friend1 = 3
+friend2 = 5
+friend3 = 8
+friend4 = 9
+people = 4
+total_ages = friend1+friend2+friend3+friend4
+print("Average age of a group of 4 is", total_ages//people)
+
+# List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+
+num_one = 12
+num_two = 7
+print("Addition(+):", num_one+num_two)
+print("Subtraction(-):", num_one-num_two)
+print("Multiplication(*):", num_one*num_two)
+print("Division(/):", num_one/num_two)
+print("Exponents(**):", num_one**num_two)
+print("Integer Division(//):", num_one//num_two)
+print("Modulo(%):", num_one%num_two)
+    # Integer Division will only give you a whole number
+    # Floats in programing is a number with decimals 
+    # Modulo gives you the remainder of your division
+# List ALL of the different assignment operators (Give me the symbols and what it does)
+    #
+
+# Why are expressions so important in programming?
+    #
