4723c054ffc27459bbb110fb51cce7e52df69631
celestebowerbank <celestebowerbank@ucas-udu.net>
2025-09-08 14:41:30 -0600

    Work in class: string notes

--- Notes/string_notes.py
+++ Notes/string_notes.py
@@ -1,3 +1,36 @@
 # CB 7th String Notes
 
 print("I did it!")
+
+# What makes something a string?
+# A set of symbols held together by "" or '' (have to be the same throughout the code)
+#Examples
+first_name = input("What is your first name?:\n").strip().title()
+
+last_name = input("What is your last name?:\n").strip().title()
+
+full_name = first_name + " "+ last_name
+
+sentence = '          The quick brown fox jumps over the lazy dog    ' 
+print (sentence.strip())
+
+print("Welcome to my program", full_name + "!")
+
+
+# Why do we have strings?
+
+# How do stupid proofing and sanitization relate to each other?
+
+# What is debugging?
+
+# How do you debug the different types of errors?
+
+# find()
+
+# concatenate (add)
+
+# upper()
+
+# lower()
+
+# strip()
