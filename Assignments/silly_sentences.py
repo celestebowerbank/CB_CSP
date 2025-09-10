44a0956eae98152960079d740cd85de8c267b4f3
celestebowerbank <celestebowerbank@ucas-udu.net>
2025-09-10 14:35:48 -0600

    Done!

--- Assignments/silly_sentences.py
+++ Assignments/silly_sentences.py
@@ -1,2 +1,15 @@
 # Cb 7th Silly Sentences
 
+name = input("What is your name:\n")
+print("Hello", name)
+
+word_one = input("Tell me a plural noun:\n")
+word_two = input("Tell me a last name:\n")
+word_three = input ("Tell me a adjective:\n")
+word_four = input ("Tell me a noun:\n")
+
+print("A one-act play to be performed by", word_one)
+
+print("Thank you very much for seeing me, doctor", word_two,"on such",word_three,"notice")
+
+print("What seems to be your problem young",word_four)
