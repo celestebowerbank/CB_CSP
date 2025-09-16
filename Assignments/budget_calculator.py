d9ccab2d3135d989a9bcd40779c1b065f108e273
celestebowerbank <celestebowerbank@ucas-udu.net>
2025-09-16 14:34:49 -0600

    Ahhhhhhhh

--- Assignments/budget_calculator.py
+++ Assignments/budget_calculator.py
@@ -13,7 +13,7 @@ savings = (1000 - (296+174+232+60))
 rent_percent = (rent/income) * 100
 utilities_percent = (utilities/income) * 100
 groceries_percent = (grocries/income) * 100
-transportation_percentsge = (trasportation/income) * 100
+transportation_percentsge = (transportation/income) * 100
 savings = (income *.10)
 leftover = income - ( rent + utilities + grocries + transportation + savings )
 
