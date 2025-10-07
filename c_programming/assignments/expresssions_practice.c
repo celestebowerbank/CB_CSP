23dbd29f53fea14ca5fc251642fb443901c4fee2
celestebowerbank <celeste.bowerbank@ucas-edu.net>
2025-10-07 12:10:45 -0600

    expresssions_practice.c

--- c_programming/assignments/expresssions_practice.c
+++ c_programming/assignments/expresssions_practice.c
@@ -1,1 +1,16 @@
+// CB 7th Expressions Practice
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    printf("1) %.0f\n", 7 - 24/8*4 + 6);
+    printf("2) %.0f\n", 18/3 - 7 + 2*5);
+    printf("3) %.0f\n", 6*4/12 + 72/8 - 9);
+    printf("4) %.0f\n", (17 - 6/2) + 4*3);
+    printf("5) %.0f\n", -2*(1*4 - 2/2) + (6 + 2 - 3));
+    printf("6) %.0f\n", -1*((3 - 4*7)/5) - 2*24/6);
+    printf("7) %.0f\n", (3*pow(5,2)/15) - (5 - pow(2,2)));
+    printf("8) %.0f\n", (pow(1,4)*pow(2,2)+pow(3,3)) - pow(2,5)/4);
+    printf("9) %.0f\n", pow((22/2 - 2*5),2) + pow((4 - 6/6),2));
+    return 0;
+}
