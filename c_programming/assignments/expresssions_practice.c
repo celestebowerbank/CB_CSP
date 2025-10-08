fd86206a0018fa03a74aa3228bff205ed8547514
celeste.bowerbank <celeste.bowerbank@ucas-edu.net>
2025-10-08 14:29:17 -0600

    Yay

--- c_programming/assignments/expresssions_practice.c
+++ c_programming/assignments/expresssions_practice.c
@@ -9,8 +9,8 @@ int main() {
     printf(" %d\n", (17 - 6/2) + 4*3);
     printf(" %d\n", -2*(1*4 - 2/2) + (6 + 2 - 3));
     printf(" %d\n", -1*((3 - 4*7)/5) - 2*24/6);
-    printf(" %d\n", (3*pow(5,2)/15) - (5 - pow(2,2)));
-    printf(" %d\n", (pow(1,4)*pow(2,2)+pow(3,3)) - pow(2,5)/4);
-    printf(" %d\n", pow((22/2 - 2*5),2) + pow((4 - 6/6),2));
+    printf(" %.0f\n", (3*pow(5,2)/15) - (5 - pow(2,2)));
+    printf(" %.0f\n", (pow(1,4)*pow(2,2)+pow(3,3)) - pow(2,5)/4);
+    printf(" %.0f\n", pow((22/2 - 2*5),2) + pow((4 - 6/6),2));
     return 0;
 }
