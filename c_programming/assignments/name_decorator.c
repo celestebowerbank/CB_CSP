21338831a7a3237906d53989a212c56f556f6377
celeste.bowerbank <celeste.bowerbank@ucas-edu.net>
2025-10-10 14:21:41 -0600

    hehe

--- c_programming/assignments/name_decorator.c
+++ c_programming/assignments/name_decorator.c
@@ -4,16 +4,9 @@
 int main() {
 
     printf("Enter your name: ");
+    char name[50];
     scanf("%49s", name); 
-
     printf("<<< %s >>>\n", name);
-    printf("((( %s )))\n", name);
-    printf("--- %s ---\n", name);
-    printf(":) :) %s :) :)\n", name);
-    printf("### %s ###\n", name);
-    printf("+++ %s +++\n", name);
-    printf("~~~ %s ~~~\n", name);
-    printf("=== %s ===\n", name);
 
     return 0;
 }
