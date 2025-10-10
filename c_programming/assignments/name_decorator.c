c5786dbee846591676f4d398fe50c0f988f6e7df
celestebowerbank <celeste.bowerbank@ucas-edu.net>
2025-10-09 18:15:38 -0600

    Update name_decorator.c

--- c_programming/assignments/name_decorator.c
+++ c_programming/assignments/name_decorator.c
@@ -1,4 +1,19 @@
 // CB 7th Name Decorator
 #include <stdio.h>
-#include <string.h>
 
+int main() {
+
+    printf("Enter your name: ");
+    scanf("%49s", name); 
+
+    printf("<<< %s >>>\n", name);
+    printf("((( %s )))\n", name);
+    printf("--- %s ---\n", name);
+    printf(":) :) %s :) :)\n", name);
+    printf("### %s ###\n", name);
+    printf("+++ %s +++\n", name);
+    printf("~~~ %s ~~~\n", name);
+    printf("=== %s ===\n", name);
+
+    return 0;
+}
