936d271cdb5c3764cbf2451f87fa3f3f91482eb2
celestebowerbank <celeste.bowerbank@ucas-edu.net>
2025-10-10 20:39:27 -0600

    Update old_enough.c

--- c_programming/assignments/old_enough.c
+++ c_programming/assignments/old_enough.c
@@ -1,2 +1,27 @@
 // CB 7th Old Enough
 #include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("How old are you: ");
+    scanf("%d", &age);
+
+    if (age >= 18) {
+        printf("You are old enough to vote!\n");
+    }
+    else if (age >= 16) {
+        printf("You are old enough to drive!\n");
+    }
+    else if (age >= 15) {
+        printf("You are old enough to get a learners permit!\n");
+    }
+    else if (age >= 5) {
+        printf("You are old enough to go to school!\n");
+    }
+    else {
+        printf("You are not old enough for any of these yet!\n");
+    }
+
+    return 0;
+}
